0704fc6888ae5f063650324dda5cd71150c1a7ff
jason jx wang <1034696975@qq.com>
2023-05-17 13:25:39 +0800

    课程表

--- src/main/java/com/pdai/javafx/app/poto/Schedule.java
+++ src/main/java/com/pdai/javafx/app/poto/Schedule.java
@@ -1,4 +1,16 @@
 package com.pdai.javafx.app.poto;
 
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
 public class Schedule {
+
+    private Map<String,String> Monday;
+    private Map<String,String> Tuesday;
+    private Map<String,String> Wednesday;
+    private Map<String,String> Thursday;
+    private Map<String,String> Friday;
+
 }
